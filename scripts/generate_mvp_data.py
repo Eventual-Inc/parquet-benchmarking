8e35d86ab84a0088ce96e4bcef8d31455ad7dc82
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-07-05 17:03:15 -0700

    Generate MVP parquet file

---
+++ scripts/generate_mvp_data.py
@@ -0,0 +1,32 @@
+import argparse
+import pyarrow as pa
+import pyarrow.parquet as papq
+
+
+def generate_pyarrow(
+    dest: str,
+):
+    tbl = pa.Table.from_pydict({
+        "a": pa.array(list(range(100)), type=pa.int64()),
+        "b": pa.array([chr(i) * i for i in range(100)], type=pa.string()),
+    })
+    papq.write_table(
+        tbl,
+        dest,
+        row_group_size=10,
+        version="1.0",  # Use minimal set of logical types
+        use_dictionary=False,  # No dictionary encoding
+        compression="NONE",  # No compression
+        write_statistics=False,  # No stats either
+        column_encoding="PLAIN",
+        data_page_version="1.0",
+        write_batch_size=2,  # Smaller batches to write smaller pages
+        data_page_size=16,  # 16 bytes per page forces multiple pages per chunk
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("dest", help="Destination for Parquet file")
+    args = parser.parse_args()
+    generate_pyarrow(args.dest)
