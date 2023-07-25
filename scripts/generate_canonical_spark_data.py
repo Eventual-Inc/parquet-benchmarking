ace2cc4fff43db8db6ef4144f4840876c46f8cf4
Jay Chia <jaychia94@gmail.com@users.noreply.github.com>
2023-07-25 13:00:10 -0700

    Add more specific column types with non-uniform distributions

--- scripts/generate_canonical_spark_data.py
+++ scripts/generate_canonical_spark_data.py
@@ -5,22 +5,17 @@
 3. Uses Spark to write the data
 """
 
-import sys
-import pyspark
-import random
+import decimal
 import string
 import dataclasses
 import numpy as np
 from pyspark.sql.functions import pandas_udf, explode
 import pandas as pd
-import pyarrow as pa
 from pyspark.sql import SparkSession
-from pyspark.sql.types import ArrayType, StructType, StructField, StringType, IntegerType, LongType, DataType as SparkDataType
+from pyspark.sql.types import ArrayType, StructType, StructField, StringType, IntegerType, DecimalType, LongType, DataType as SparkDataType
 
 
-
-
-@dataclasses.dataclass(frozen=True)
+@dataclasses.dataclass
 class ColumnSpec:
     name: str
 
@@ -55,8 +50,7 @@ class ColumnSpec:
         return self.random_generate(size)
 
 
-
-@dataclasses.dataclass(frozen=True)
+@dataclasses.dataclass
 class SchemaSpec:
     fields: list[ColumnSpec]
 
@@ -64,55 +58,94 @@ class SchemaSpec:
         return StructType([StructField(f.name, f.spark_type()) for f in self.fields])
 
 
-class IntegerColumn(ColumnSpec):
+@dataclasses.dataclass
+class Int64SmallValuesColumn(ColumnSpec):
 
     def spark_type(self) -> SparkDataType:
-        return IntegerType()
+        return LongType()
 
     def generate_unique_values(self, num: int) -> np.ndarray:
         return np.arange(num)
 
     def random_generate(self, size: int) -> np.ndarray:
-        min_int32 = -2_147_483_648
-        max_int32 = 2_147_483_647
-        return np.random.randint(min_int32, max_int32, size=(size,))
+        min_int64 = -(2**63)
+        max_int64 = (2**63) - 1
+        return np.random.randint(min_int64, max_int64, size=(size,))
+    
+
+@dataclasses.dataclass
+class DecimalPctColumn(ColumnSpec):
+
+    def spark_type(self) -> SparkDataType:
+        return DecimalType(10, 2)
+
+    def generate_unique_values(self, num: int) -> np.ndarray:
+        return [round(decimal.Decimal(i), 2) for i in np.random.rand(num)]
+
+    def random_generate(self, size: int) -> np.ndarray:
+        return [round(decimal.Decimal(i), 2) for i in np.random.rand(size,)]
 
 
-class StringColumn(ColumnSpec):
+@dataclasses.dataclass
+class StringCategoricalColumn(ColumnSpec):
 
-    min_bytes_length: int = 32
-    max_bytes_length: int = 64
+    min_bytes_length: int = 8
+    max_bytes_length: int = 16
+
+    CHAR_CHOICES = list(string.ascii_letters)
 
     def spark_type(self) -> SparkDataType:
         return StringType()
 
     def generate_unique_values(self, num: int) -> np.ndarray:
-        str_lengths = np.random.randint(self.min_bytes_length, self.max_bytes_length, size=(num,))
+        str_lengths = (
+            np.ones((num,)).astype(np.int64) * self.min_bytes_length
+            if self.min_bytes_length == self.max_bytes_length
+            else np.random.randint(self.min_bytes_length, self.max_bytes_length, size=(num,))
+        )
         strings = set()
         while len(strings) != num:
             str_len = str_lengths[len(strings)]
-            generated = "".join(np.random.choice(CHAR_CHOICES, replace=True, size=(str_len,)))
+            generated = "".join(np.random.choice(StringCategoricalColumn.CHAR_CHOICES, replace=True, size=(str_len,)))
             strings.add(generated)
         return np.array(list(strings))
 
     def random_generate(self, size: int) -> np.ndarray:
-        str_lengths = np.random.randint(self.min_bytes_length, self.max_bytes_length, size=(size,))
+        str_lengths = (
+            np.ones((size,)).astype(np.int64) * self.min_bytes_length
+            if self.min_bytes_length == self.max_bytes_length
+            else np.random.randint(self.min_bytes_length, self.max_bytes_length, size=(size,))
+        )
         return np.array(list(map(
-            lambda str_len: "".join(np.random.choice(CHAR_CHOICES, replace=True, size=(str_len,))),
+            lambda str_len: "".join(np.random.choice(StringCategoricalColumn.CHAR_CHOICES, replace=True, size=(str_len,))),
             str_lengths,
         )))
 
 
 NUM_ROWS = 2_440_000
 SCHEMA_SPEC = SchemaSpec([
-    IntegerColumn("int64"),
-    StringColumn("categorical"),
+    Int64SmallValuesColumn(name="int64_unique-1", num_unique_values=1),
+    Int64SmallValuesColumn(name="int64_unique-10", num_unique_values=10),
+    Int64SmallValuesColumn(name="int64_unique-100", num_unique_values=100),
+    Int64SmallValuesColumn(name="int64_unique-1000", num_unique_values=1000),
+    Int64SmallValuesColumn(name="int64_unique-10000", num_unique_values=10000),
+    Int64SmallValuesColumn(name="int64_unique-100000", num_unique_values=100000),
+    Int64SmallValuesColumn(name="int64_unique-1000000", num_unique_values=1000000),
+    StringCategoricalColumn(name="small_categoricals_unique-1", num_unique_values=1),
+    StringCategoricalColumn(name="small_categoricals_unique-10", num_unique_values=10),
+    StringCategoricalColumn(name="small_categoricals_unique-100", num_unique_values=100),
+    StringCategoricalColumn(name="small_categoricals_unique-1000", num_unique_values=1000),
+    StringCategoricalColumn(name="small_categoricals_unique-10000", num_unique_values=10000),
+    StringCategoricalColumn(name="small_categoricals_unique-100000", num_unique_values=100000),
+    StringCategoricalColumn(name="small_categoricals_unique-1000000", num_unique_values=1000000),
+    DecimalPctColumn(name="decimal_p-2_s-2_unique-1", num_unique_values=1),
+    DecimalPctColumn(name="decimal_p-2_s-2_unique-10", num_unique_values=10),
+    DecimalPctColumn(name="decimal_p-2_s-2_unique-100", num_unique_values=100),
 ])
 NUM_PARTITIONS = 200
 NUM_ROWS_PER_PARTITION = int(NUM_ROWS // NUM_PARTITIONS)
 MIN_STRING_LENGTH = 32
 MAX_STRING_LENGTH = 64
-CHAR_CHOICES = list(string.ascii_letters)
 
 
 def generate_data(schema_spec: SchemaSpec, seed: int) -> dict[str, list]:
@@ -127,15 +160,8 @@ def generate_data_per_partition(part_ids: pd.Series) -> pd.Series:
     return pd.Series(data)
 
 
-def main(
-    dest: str,
-    row_group_size_bytes: int = 134_217_728,
-    page_size_bytes: int = 1_048_576,
-):
+def main(dest: str):
     spark = SparkSession.builder.appName("DataGenerator").getOrCreate()
-    # spark_schema = StructType([StructField(name, _get_spark_type(daft_type)) for name, daft_type in data_options.daft_schema.items()])
-    # python_data = _generate_data(data_options)
-    # df = spark.createDataFrame(data=python_data, schema=spark_schema)
     rdd = spark.sparkContext.parallelize([(i,) for i in range(NUM_PARTITIONS)])
     df = rdd.toDF(["part_id"])
     df = df.select(explode(generate_data_per_partition("part_id")).alias("data"))
@@ -144,8 +170,6 @@ def main(
     # Full list of available options: https://github.com/apache/parquet-mr/blob/master/parquet-hadoop/README.md
     df.repartition(1) \
         .write \
-        .option("parquet.block.size", row_group_size_bytes) \
-        .option("parquet.page.size", page_size_bytes) \
         .parquet(dest)
 
 
